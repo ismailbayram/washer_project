bf8515c91cadcd34828284c831c6a85a440beb8b
ismail bayram <ismailbayram.ce@gmail.com>
2019-06-20 20:43:57 +0300

    [#3] removed checking wheter view.permission_actions is None or not

--- api/permissions.py
+++ api/permissions.py
@@ -14,7 +14,7 @@ class IsAdminUser(BasePermission):
 
 class HasGroupPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_superuser or view.permission_groups is None:
+        if request.user.is_superuser:
             return True
         required_groups = view.permission_groups.get(view.action, None)
         if not required_groups:
