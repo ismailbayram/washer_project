1bad95f5d45d203d0601a7ce8e0cee50492db194
ismail bayram <ismailbayram.ce@gmail.com>
2019-06-29 00:11:07 +0300

    [#5] added day, week and config serializer

--- stores/resources/serializers.py
+++ stores/resources/serializers.py
@@ -1,7 +1,48 @@
+from django.utils.translation import ugettext_lazy as _
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from address.resources.serializers import AddressDetailedSerializer
 from stores.models import Store
+
+
+class DaySerializer(serializers.Serializer):
+    start = serializers.TimeField(format="[HH:[MM]]", allow_null=True)
+    end = serializers.TimeField(format="[HH:[MM]]", allow_null=True)
+    # TODO: test
+
+    def validate(self, attrs):
+        start = attrs['start']
+        end = attrs['end']
+
+        if (start and not end) or (not start and end):
+            raise ValidationError(_('Başlangıç ve bitiş saatleri verilmeli.'))
+
+        return attrs
+
+
+class WeekSerializer(serializers.Serializer):
+    monday = DaySerializer()
+    tuesday = DaySerializer()
+    wednesday = DaySerializer()
+    thursday = DaySerializer()
+    friday = DaySerializer()
+    saturday = DaySerializer()
+    sunday = DaySerializer()
+
+
+class OpeningHoursSerializer(WeekSerializer):
+    pass
+
+
+class ReservationHoursSerializer(WeekSerializer):
+    pass
+
+
+class ConfigSerializer(serializers.Serializer):
+    opening_hours = OpeningHoursSerializer(default={})
+    reservation_hours = ReservationHoursSerializer(default={})
 
 
 class StoreSerializer(serializers.ModelSerializer):
@@ -9,7 +50,7 @@ class StoreSerializer(serializers.ModelSerializer):
     rating = serializers.ReadOnlyField()
     is_approved = serializers.ReadOnlyField()
     washer_profile = serializers.PrimaryKeyRelatedField(read_only=True)
-    # TODO: config serializer
+    config = ConfigSerializer()
 
     class Meta:
         model = Store
