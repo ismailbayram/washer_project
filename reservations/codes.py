74442790955f594890f85c91dbf1c4db7e80772a
İsmail Bayram <ismailbayram.ce@gmail.com>
2019-07-11 15:43:26 +0300

    [#24] reservation app files created

---
+++ reservations/codes.py
@@ -0,0 +1,3 @@
+from django.utils.translation import ugettext_lazy as _
+
+reservations_100_0 = {"code": "reservations_100_0", "message": _("")}
---
+++ reservations/exceptions.py
@@ -0,0 +1,6 @@
+from api.exceptions import ProjectBaseException
+from reservations import codes
+
+
+# class PeriodIsRequiredException(ProjectBaseException):
+#     code = codes.product_100_0
