c11d8a76ef427ae21603cf890640dbea2cb42ded
İsmail Bayram <ismailbayram.ce@gmail.com>
2019-07-12 17:02:54 +0300

    [#24] added create_week_from_config service method

--- reservations/service.py
+++ reservations/service.py
@@ -1,15 +1,20 @@
-import random
 import datetime
+import pytz
+
+from django.db.transaction import atomic
+from django.utils.crypto import get_random_string
+from django.conf import settings
 
 from reservations.models import Reservation
-from reservations.enums import ReservationStatus
+
+
+timezone = pytz.timezone(settings.TIME_ZONE)
 
 
 class ReservationService:
     # TODO: check basket is empty
     def _generate_reservation_number(self):
-        # TODO: make this str
-        number = str(random.randint(10_000_000_000, 99_999_999_999))
+        number = get_random_string(length=10).upper()
         if Reservation.objects.filter(number=number).exists():
             return self._generate_reservation_number()
         return number
@@ -30,3 +35,29 @@ class ReservationService:
                                                      number=self._generate_reservation_number())
 
         return reservation
+
+    @atomic
+    def create_week_from_config(self, store):
+        config = store.config['reservation_hours']
+        period = store.product_set.filter(is_primary=True).order_by('-period').first().period
+
+        for k in range(1, 8):
+            day_datetime = datetime.datetime.today() + datetime.timedelta(days=k)
+            day = day_datetime.strftime("%A").lower()
+
+            start = config[day]['start']
+            end = config[day]['end']
+            if start is None:
+                continue
+
+            start_time = datetime.datetime.strptime(start, "%H:%M")
+            end_time = datetime.datetime.strptime(end, "%H:%M")
+            diff = (end_time - start_time).seconds / 60
+            period_count = int(diff / period)
+
+            start_datetime = day_datetime.replace(hour=start_time.hour, minute=start_time.minute,
+                                                  second=0)
+            for p in range(period_count):
+                start_dt = start_datetime + datetime.timedelta(minutes=(p * period))
+                start_dt = timezone.localize(start_dt)
+                self.create_reservation(store=store, start_datetime=start_dt, period=period)
