82de239fa163e5a5401184f98652efcf926de658
kadir can çetin <kadircancetin@gmail.com>
2019-07-28 19:06:25 +0300

    [#35] Change header strings to group type

--- notifications/resources/views.py
+++ notifications/resources/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from notifications.models import Notification
 from notifications.resources.serializers import NotificationSerializer
+from users.enums import GroupType
 
 
 class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
@@ -15,15 +16,15 @@ class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
         queryset = self.queryset
         profile_type = self.request.META.get("HTTP_X_PROFILE_TYPE")
 
-        if profile_type not in ('washer', 'customer', 'worker'):
+        if profile_type not in (GroupType.worker.value, GroupType.customer.value, GroupType.washer.value):
             # TODO log here
             raise NotFound()
 
-        if profile_type == 'washer':
+        if profile_type == GroupType.washer.value:
             queryset = queryset.filter(washer_profile=self.request.user.washer_profile)
-        elif profile_type == 'customer':
+        elif profile_type == GroupType.customer.value:
             queryset = queryset.filter(customer_profile=self.request.user.customer_profile)
-        elif profile_type == 'worker':
+        elif profile_type == GroupType.worker.value:
             queryset = queryset.filter(worker_profile=self.request.user.worker_profile)
 
         return super().filter_queryset(queryset)
