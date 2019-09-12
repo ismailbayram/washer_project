db1e49d50101f3da8ac4a994654633af103b8136
ismail bayram <ismailbayram.ce@gmail.com>
2019-09-12 23:54:43 +0300

    [#55] added name filters

--- admin/users/filters.py
+++ admin/users/filters.py
@@ -8,6 +8,8 @@ class UserFilterSet(filters.FilterSet):
     is_customer = filters.BooleanFilter(method='filter_is_customer')
     is_worker = filters.BooleanFilter(method='filter_is_worker')
     is_washer = filters.BooleanFilter(method='filter_is_washer')
+    first_name = filters.CharFilter(lookup_expr='icontains')
+    last_name = filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = User
@@ -16,15 +18,15 @@ class UserFilterSet(filters.FilterSet):
 
     def filter_is_customer(self, qs, name, value):
         if bool(value):
-            return qs.filter()
+            return qs.filter(groups__name=GroupType.customer.value)
         return qs.filter(~Q(groups__name=GroupType.customer.value))
 
     def filter_is_worker(self, qs, name, value):
         if bool(value):
-            return qs.filter()
+            return qs.filter(groups__name=GroupType.worker.value)
         return qs.filter(~Q(groups__name=GroupType.worker.value))
 
     def filter_is_washer(self, qs, name, value):
         if bool(value):
-            return qs.filter()
+            return qs.filter(groups__name=GroupType.washer.value)
         return qs.filter(~Q(groups__name=GroupType.washer.value))
