de7ec11adf3cfade1163bf3b1739c27fd79eb8e2
ismail bayram <ismailbayram.ce@gmail.com>
2019-09-12 21:38:26 +0300

    [#55] added UserFilterSet

--- admin/users/filters.py
+++ admin/users/filters.py
@@ -1,10 +1,30 @@
 from django_filters import rest_framework as filters
-
+from django.db.models import Q
 from users.models import User
+from users.enums import GroupType
 
 
 class UserFilterSet(filters.FilterSet):
+    is_customer = filters.BooleanFilter(method='filter_is_customer')
+    is_worker = filters.BooleanFilter(method='filter_is_worker')
+    is_washer = filters.BooleanFilter(method='filter_is_washer')
+
     class Meta:
         model = User
-        fields = ('first_name', 'last_name')
-        # 'is_customer', 'is_worker', 'is_washer'
+        fields = ('first_name', 'last_name', 'is_customer', 'is_worker', 'is_washer',
+                  'is_staff', 'is_superuser')
+
+    def filter_is_customer(self, qs, name, value):
+        if bool(value):
+            return qs.filter()
+        return qs.filter(~Q(groups__name=GroupType.customer.value))
+
+    def filter_is_worker(self, qs, name, value):
+        if bool(value):
+            return qs.filter()
+        return qs.filter(~Q(groups__name=GroupType.worker.value))
+
+    def filter_is_washer(self, qs, name, value):
+        if bool(value):
+            return qs.filter()
+        return qs.filter(~Q(groups__name=GroupType.washer.value))
--- admin/users/serializers.py
+++ admin/users/serializers.py
@@ -10,6 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('pk', 'date_joined', 'last_login', 'first_name', 'last_name',
                   'phone_number', 'is_active', 'is_customer', 'is_washer',
                   'is_worker')
+        extra_kwargs = {
+            'phone_number': {'read_only': True},
+            'date_joined': {'read_only': True},
+        }
 
 
 class LoginSerializer(serializers.Serializer):
