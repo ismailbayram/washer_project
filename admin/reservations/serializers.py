7b18552c0f7fc232447908f49fc87ee0c053688a
kadir can çetin <kadircancetin@gmail.com>
2019-09-14 20:07:21 +0300

    [#40] Created CancellationReason admin ModelViewSet

---
+++ admin/reservations/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from reservations.models import CancellationReason
+
+
+class CancellationReasonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CancellationReason
+        fields = ('pk', 'reason', 'is_active')
---
+++ admin/reservations/views.py
@@ -0,0 +1,19 @@
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from admin.reservations.serializers import CancellationReasonSerializer
+from reservations.models import CancellationReason
+
+
+
+class ReservationCancellationAdminViewSet(ModelViewSet):
+    queryset = CancellationReason.objects.all()
+    serializer_class = CancellationReasonSerializer
+    permission_classes = (IsAdminUser, )
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+        return Response({}, status.HTTP_200_OK)
