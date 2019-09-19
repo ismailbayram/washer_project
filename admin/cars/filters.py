6397fae69fb8334d4723190b98ec15ee683af604
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-09-19 22:08:03 +0300

    [#63] Fix mistakes in car serializer

--- admin/cars/filters.py
+++ admin/cars/filters.py
@@ -6,8 +6,8 @@ from cars.models import Car
 class CarFilterSet(filters.FilterSet):
     licence_plate = filters.CharFilter(lookup_expr='icontains')
     car_type = filters.CharFilter(field_name="car_type", lookup_expr="exact")
-    costumer_profile = filters.CharFilter(field_name='customer_profile', lookup_expr='icontains')
+    user = filters.CharFilter(field_name="customer_profile__user")
 
     class Meta:
         model = Car
-        fields = ('licence_plate', 'car_type', 'is_selected', 'is_active', 'costumer_profile', )
+        fields = ('licence_plate', 'car_type', 'is_selected', 'is_active', 'user', )
--- admin/cars/serializers.py
+++ admin/cars/serializers.py
@@ -3,32 +3,23 @@ from rest_framework import serializers
 from cars.models import Car
 from cars.enums import CarType
 from api.fields import EnumField
-from users.models import CustomerProfile, User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('pk', 'first_name', 'last_name', 'phone_number',)
-        extra_kwargs = {
-            'phone_number': {'read_only': True},
-            'first_name': {'read_only': True},
-            'last_name': {'read_only': True},
-        }
+from users.models import User
 
 
 class CustomerProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    phone_number = serializers.CharField(source='user.phone_number')
 
     class Meta:
         model = User
-        fields = ('user', )
+        fields = ('pk', 'first_name', 'last_name', 'phone_number')
 
 
 class CarSerializer(serializers.ModelSerializer):
     car_type = EnumField(enum=CarType)
-    customer_profile = CustomerProfileSerializer()
+    user = CustomerProfileSerializer(source='customer_profile')
 
     class Meta:
         model = Car
-        fields = ('pk', 'customer_profile', 'licence_plate', 'car_type', 'is_selected', 'is_active', )
+        fields = ('pk', 'user', 'licence_plate', 'car_type', 'is_selected', 'is_active', )
