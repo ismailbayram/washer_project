4a4dcb54b8928d51dabe6b3c030e0335c713c069
İsmail Bayram <ismail.bayram@akinon.com>
2020-01-16 10:09:19 +0300

    pipeline fix

--- scripts/run_pytests_bitbucket_pipeline.sh
+++ scripts/run_pytests_bitbucket_pipeline.sh
@@ -1,32 +1,3 @@
-.sh
 #!/usr/bin/env bash
 
-file="washer_project/settings_test.py"
-if [ -f "$file" ]; then
-    echo "settings_test exists..."
-else
-    cat > $file <<EOF
-DEBUG = True
-
-LOGIN_URL = 'rest_framework:login'
-LOGOUT_URL = 'rest_framework:logout'
-
-CACHE_BACKEND_URL = 'redis://127.0.0.1:6379/0'
-
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/1'
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": CACHE_BACKEND_URL,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-
-EOF
-
-fi
-
-pytest --durations=10
+python manage.py test --settings=washer_project.settings_test
