1efd93d4628bd9425e7c9f9212153f4ce69003b5
İsmail Bayram <ismail.bayram@akinon.com>
2020-01-16 09:58:20 +0300

    pipeline

---
+++ scripts/run_pytests_bitbucket_pipeline.sh
@@ -0,0 +1,32 @@
+.sh
+#!/usr/bin/env bash
+
+file="oms_project/settings_local.py"
+if [ -f "$file" ]; then
+    echo "settings_test exists..."
+else
+    cat > $file <<EOF
+DEBUG = True
+
+LOGIN_URL = 'rest_framework:login'
+LOGOUT_URL = 'rest_framework:logout'
+
+CACHE_BACKEND_URL = 'redis://127.0.0.1:6379/0'
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/1'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": CACHE_BACKEND_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+EOF
+
+fi
+
+pytest --durations=10
